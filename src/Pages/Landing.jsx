3cc29cdc2703b9e7aeaec9ee2c4b3a69a84ec467
unknown <madhinabanu7866@gmail.com>
2024-11-26 21:26:29 -0800

    Uploaded Weather App

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,17 @@
+import Weather from "./Weather";
+import { useLocation } from "react-router-dom"
+
+
+function Landing()
+{    const data = useLocation()
+    console.log(data.state.user)
+
+    return(
+        <>
+        <Weather/>
+        </>
+    )
+}
+
+
+export default Landing
